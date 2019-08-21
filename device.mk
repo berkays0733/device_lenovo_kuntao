c4f02c371ec289f5473fcfd115ce3a26605e1de0
Murat Kozan <muratkozan350@gmail.com>
2019-08-21 23:15:14 +0300

    kuntao: Enable colt overlay

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-colt
 
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
